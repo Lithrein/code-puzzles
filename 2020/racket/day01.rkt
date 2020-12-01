2a1bdc5370e18a0e3499a6f51a912c477cfdda76
Lithrein <lithrein.site@gmail.com>
2020-12-01 12:41:23 +0100

    day01: naive (painful) solution (racket)

---
+++ 2020/racket/day01.rkt
@@ -0,0 +1,28 @@
+#lang racket
+
+(define (make-list-from-input in)
+  (let ([val (read in)])
+    (if (eof-object? val)
+        '()
+         (cons val (make-list-from-input in)))))
+
+(define (part1 lst)
+  (let ([res 0])
+    (for ([i lst])
+      (for ([j lst])
+        (cond [ (= 2020 (+ i j)) (set! res (* i j))])))
+    res))
+
+(define (part2 lst)
+  (let ([res 0])
+    (for ([i lst])
+      (for ([j lst])
+        (for ([k lst])
+          (cond [ (= 2020 (+ i j k)) (set! res (* i j k))]))))
+  res))
+
+(define in (open-input-file "inputs/day01"))
+(define lst (make-list-from-input in))
+(write (part1 lst))
+(display "\n")
+(write (part2 lst))
