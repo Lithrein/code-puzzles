a417d502eed0b5a6b0faf4b05c36e15b28910867
Lithrein <lithrein.site@gmail.com>
2020-12-03 18:53:29 +0100

    day03: improve readability (racket)

--- 2020/racket/day03.rkt
+++ 2020/racket/day03.rkt
@@ -5,23 +5,21 @@
     (lambda (str) (list->vector (string->list str)))
       (file->lines file-name))))
 
-(define (count-tree array x y)
- (let ([height (vector-length array)]
-       [width (vector-length (vector-ref array 0))])
+(define (count-tree array dx dy)
+  (define height (vector-length array))
+  (define width (vector-length (vector-ref array 0)))
   (define (walk-cnt i j)
-    (if (< i height)
-       (if (char=? (vector-ref (vector-ref array i) j) #\#)
-           (+ 1 (walk-cnt (+ i x) (modulo (+ j y) width)))
-           (walk-cnt (+ i x) (modulo (+ j y) width)))
-       0))
-  (walk-cnt 0 0)))
-
-(define input (load-map-from "inputs/day03"))
+    (cond
+      [ (< i height)
+           (+ (walk-cnt (+ i dx) (modulo (+ j dy) width))
+           (if (char=? (vector-ref (vector-ref array i) j) #\#) 1 0))]
+      [else 0]))
+  (walk-cnt 0 0))
 
 (define (day03 array slopes)
-  (foldl
-    (lambda (slope res) (* res (count-tree array (car slope) (cadr slope))))
-    1 slopes))
+  (define/match (aux slope acc)
+    [(`(,dx ,dy) _) (* acc (count-tree array dx dy))])
+  (foldl aux 1 slopes))
 
 (define (part1 array)
   (day03 array '((1 3))))
@@ -29,6 +27,5 @@
 (define (part2 array)
   (day03 array '((1 1) (1 3) (1 5) (1 7) (2 1))))
 
-(display (part1 input))
-(display "\n")
-(display (part2 input))
+(define input (load-map-from "inputs/day03"))
+(printf "part1: ~a~npart2: ~a~n" (part1 input) (part2 input))
