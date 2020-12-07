ee5558d475e821114eb464284dfee6642db32249
Lithrein <lithrein.site@gmail.com>
2020-12-07 18:29:29 +0100

    day07: clean up (ruby)

--- 2020/ruby/day07.rb
+++ 2020/ruby/day07.rb
@@ -1,27 +1,22 @@
 #! /usr/bin/env ruby
 
-hash = {}
-input = File.open('../inputs/day07').read()
+$hash = {}
+input = File.open('../inputs/day07').read
 
-input.each_line() do |line|
+input.each_line do |line|
   key = line.match(/(.*) bags contain/)[1]
-  val = line.scan(/(\d+) (.[^,.]*(?= bag))/).map(&->a{[a[1],a[0].to_i]}).to_h
-  hash[key] = val
+  val = line.scan(/(\d+) (.[^,.]*(?= bag))/).map {|a| [a[1],a[0].to_i]}.to_h
+  $hash[key] = val
 end
 
-
-def can_shiny (bag, hash)
-  return true if hash[bag].keys.include?("shiny gold")
-  return hash[bag].keys.any? ->x{can_shiny(x, hash)}
+def can_shiny bag
+  keys = $hash[bag].keys
+  keys.include?("shiny gold") || keys.any? {|x| can_shiny x}
 end
 
-def shiny_space(key, hash)
-  res = hash[key].values.sum
-  hash[key].each do |(k,v)|
-    res = res + v * shiny_space(k,hash)
-  end
-  return res
+def shiny_space key
+  $hash[key].map {|(k,v)| v + v * shiny_space(k)}.sum
 end
 
-puts hash.keys.map(&->k{can_shiny(k,hash)}).select(&->b{b == true}).length()
-puts shiny_space("shiny gold", hash)
+puts $hash.keys.map {|k| can_shiny k}.select(&:itself).length
+puts shiny_space "shiny gold"
