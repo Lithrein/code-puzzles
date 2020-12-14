6c4a3dc3248b5ad4c9757b9f3f536447db32fccf
Lithrein <lithrein.site@gmail.com>
2020-12-14 14:44:19 +0100

    day14: add solution (ruby)

---
+++ 2020/ruby/day14.rb
@@ -0,0 +1,46 @@
+#! /usr/bin/env ruby
+
+def part1 input
+  msk, msk_val, mem = 0, 0, Hash.new(0)
+  input.each do |l|
+    if l =~ /mask/ then
+      match = l.match(/mask = (.+)/)[1]
+      msk = match.tr("X01","01").to_i 2
+      msk_val = match.tr("X","0").to_i 2
+    else
+      match = l.match(/mem\[(\d+)\] = (\d+)/)
+      loc, val = match[1].to_i, match[2].to_i
+      mem[loc] = ((~0x2000000000 & ~msk & val) | msk_val)
+    end
+  end
+  mem.values.sum
+end
+
+def part2 input
+  msk, msk_val, mem = 0, 0, Hash.new(0)
+  input.each do |l|
+    if l =~ /mask/ then
+      match = l.match(/mask = (.+)/)[1]
+      msk = match.tr("X01","01").to_i 2
+      msk_val = match.tr("X","0").to_i 2
+    else
+      match = l.match(/mem\[(\d+)\] = (\d+)/)
+      base_loc, val = (match[1].to_i | msk_val) & msk, match[2].to_i
+      tmp_msk, last_msk, locs, bp = ~msk, 0, [0], 0
+      while true do
+        bp = tmp_msk & -tmp_msk
+        break if ("%b" % bp).length > 36
+        locs = locs + locs.map(&->x{x + bp})
+        tmp_msk = ~0x2000000000 & tmp_msk & ~bp
+      end
+      locs.each do |loc|
+        mem[base_loc | loc] = val
+      end
+    end
+  end
+  mem.values.sum
+end
+
+input = File.open('../inputs/day14').readlines
+p part1(input)
+p part2(input)
