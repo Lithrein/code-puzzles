23d17d7f194e85ce630c3f291184c295223bdce7
Lithrein <lithrein.site@gmail.com>
2020-12-19 23:03:46 +0100

    day19: part2 'works' (ruby)

--- 2020/ruby/day19.rb
+++ 2020/ruby/day19.rb
@@ -63,6 +63,38 @@ def part1 input, regex
   input.select {|l| l.match? regex}.length
 end
 
+def expend_rule2 rules, n
+  tmp = rules[n]
+  str = ""
+  if tmp == [["a"]] || tmp == [["b"]] then
+    str = tmp[0][0]
+  elsif n == "8" then
+    str += "(?:" + expend_rule2(rules,"42") + ")+"
+  elsif n == "11" then
+    a = "(?:" + expend_rule2(rules,"42") + ")"
+    b = "(?:" + expend_rule2(rules,"31") + ")"
+    str += "(?:(?:#{a}{1}#{b}{1})"           \
+        + "|" + "(?:#{a}{2}#{b}{2})"         \
+        + "|" + "(?:#{a}{3}#{b}{3}))"        \
+        + "|" + "(?:#{a}{4}#{b}{4}))"
+  else
+    left  = rules[n][0]
+    right = rules[n][1]
+
+    str += '(?:'
+    left.each do |nb|
+      str += expend_rule2(rules,nb)
+    end
+    if !right.nil? then
+      str += '|'
+      right.each do |nb|
+        str += expend_rule2(rules,nb)
+      end
+    end
+    str += ')'
+  end
+  str
+end
 
 raw_rules, input = File.open('../inputs/day19').read.split("\n\n").map { |x| x.split("\n") }
 # raw_rules, input = <<-DATA
@@ -83,4 +115,7 @@ raw_rules, input = File.open('../inputs/day19').read.split("\n\n").map { |x| x.s
 #   .map { |x| x.split("\n") }
 rules = parse_rule(raw_rules)
 regex = Regexp.new("^" + expend_rule1(rules,"0") + "$")
+puts part1(input, regex)
+regex = Regexp.new("^" + expend_rule2(rules,"0") + "$")
+p regex
 puts part1(input, regex)
