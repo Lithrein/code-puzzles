8cc6bb6fd7ace99464786397db6c0894d9e719e6
Lithrein <lithrein.site@gmail.com>
2020-12-24 23:42:18 +0100

    day24: very slow part 2 (ruby)

--- 2020/ruby/day24.rb
+++ 2020/ruby/day24.rb
@@ -17,15 +17,95 @@ def walk path
   [x,y]
 end
 
-def part1 paths
+def walk_and_flip paths
   marked = Hash.new 0
   paths.each do |path|
     pos = walk(path)
     marked[pos] += 1
   end
-  marked.values.select { |m| m % 2 == 1 }.length
+  marked.select { |h,v| v % 2 == 1 }
 end
 
+# neighbors of (0,0):
+#   - (0,1), (0,-1), (-1,1)
+#   - (1,0), (-1,0), (1,-1)
+#   - (1,1), (-1,-1) not neighbors
+#   - neighbors |(x-x0) + (y-y0)| < 2
+#       e == nese == sene
+#       w == nwsw == swnw
+def neighbors pos
+  x0, y0 = pos
+  rel_neighbors = [[0,1], [0,-1], [-1,1], [1,0], [-1,0], [1,-1]]
+
+  rel_neighbors.map { |(x,y)| [x + x0, y + y0] }
+end
+
+def next_config config
+  to_be_flipped = []
+
+  # black to flip
+  config.each do |h,v|
+    nb_black_neighbors = neighbors(h).select { |n| config[n] == 1 }.length
+    if nb_black_neighbors == 0 || nb_black_neighbors > 2 then
+      to_be_flipped += [h]
+    end
+  end
+
+  # white to flip
+  config.keys.permutation(2).each do |t1, t2|
+    candidates = neighbors(t1) & neighbors(t2)
+    candidates.each do |h|
+      if config[h] != 1 && neighbors(h).select { |n| config[n] == 1 }.length == 2 then
+        to_be_flipped += [h]
+      end
+    end
+  end
+
+  to_be_flipped.uniq!
+
+  # flip time
+  to_be_flipped.each do |h|
+    config[h] = config[h].nil? ? 1 : config[h] + 1
+  end
+
+  # only keep blacks
+  config.select { |h,v| v % 2 == 1 }
+end
+
+def next_config_n config, n
+  new_config = config
+  i = 0
+  while i < n do
+    p i
+    new_config = next_config(new_config)
+    i += 1
+  end
+  new_config
+end
+
+input = <<-DATA
+sesenwnenenewseeswwswswwnenewsewsw
+neeenesenwnwwswnenewnwwsewnenwseswesw
+seswneswswsenwwnwse
+nwnwneseeswswnenewneswwnewseswneseene
+swweswneswnenwsewnwneneseenw
+eesenwseswswnenwswnwnwsewwnwsene
+sewnenenenesenwsewnenwwwse
+wenwwweseeeweswwwnwwe
+wsweesenenewnwwnwsenewsenwwsesesenwne
+neeswseenwwswnwswswnw
+nenwswwsewswnenenewsenwsenwnesesenew
+enewnwewneswsewnwswenweswnenwsenwsw
+sweneswneswneneenwnewenewwneswswnese
+swwesenesewenwneswnwwneseswwne
+enesenwswwswneneswsenwnewswseenwsese
+wnwnesenesenenwwnenwsewesewsesesew
+nenewswnwewswnenesenwnesewesw
+eneswnwswnwsenenwnwnwwseeswneewsenese
+neswnwewnwnwseenwseesewsenwsweewe
+wseweeenwnesenwwwswnew
+DATA
+  .split "\n"
 input = File.open('../inputs/day24').read.lines.map(&:chomp)
 
 paths = input.map do |str|
@@ -36,4 +116,8 @@ paths = input.map do |str|
      .map(&:join)
 end
 
-puts part1(paths)
+init_config = walk_and_flip(paths)
+puts init_config.length # part1
+
+end_config  = next_config_n(init_config, 100)
+puts end_config.length  # part2
