a3d24e48e39f679fdcbc03c33cdcfecd5fc825c3
Lithrein <lithrein.site@gmail.com>
2020-12-29 14:35:34 +0100

    day01: more idiomatic file reading (racket)

--- 2020/racket/day01.rkt
+++ 2020/racket/day01.rkt
@@ -1,11 +1,7 @@
 #! /usr/bin/env racket
 #lang racket/base
 
-(define (make-list-from-input in)
-  (let ([val (read in)])
-    (if (eof-object? val)
-        '()
-         (cons val (make-list-from-input in)))))
+(require racket/file)
 
 (define (part1 lst)
   (for*/first ([i lst]
@@ -20,6 +16,5 @@
                #:when (= 2020 (+ i j k)))
     (* i j k)))
 
-(define in (open-input-file "../inputs/day01"))
-(define lst (make-list-from-input in))
+(define lst (file->list "../inputs/day01"))
 (printf "part1: ~a~npart2: ~a~n" (part1 lst) (part2 lst))
