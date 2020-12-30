3ad1dc7ae9723dd88bc458bbcfad6dadfca9e0c0
Lithrein <lithrein.site@gmail.com>
2020-12-30 10:52:56 +0100

    day01: add tests (racket)

--- 2020/racket/day01.rkt
+++ 2020/racket/day01.rkt
@@ -3,11 +3,19 @@
 
 (require racket/file)
 
+(module+ test
+  (require rackunit)
+  (define nums '(1721 979 366 299 675 1456)))
+
+
 (define (part1 lst)
   (for*/first ([i lst]
                [j lst]
                #:when (= 2020 (+ i j)))
     (* i j)))
+
+(module+ test
+  (check-equal? (part1 nums) 514579))
 
 (define (part2 lst)
   (for*/first ([i lst]
@@ -16,5 +24,9 @@
                #:when (= 2020 (+ i j k)))
     (* i j k)))
 
-(define lst (file->list "../inputs/day01"))
-(printf "part1: ~a~npart2: ~a~n" (part1 lst) (part2 lst))
+(module+ test
+  (check-equal? (part2 nums) 241861950))
+
+(module+ main
+  (define lst (file->list "../inputs/day01"))
+  (printf "part1: ~a~npart2: ~a~n" (part1 lst) (part2 lst)))
