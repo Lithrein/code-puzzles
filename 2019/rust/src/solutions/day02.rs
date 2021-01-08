718bff55523ce067a01c0c1c77ad2cdb8997f0ea
Lithrein <lithrein.site@gmail.com>
2021-01-08 10:35:37 +0100

    day02: a bit less ugly (rust)

--- 2019/rust/src/solutions/day02.rs
+++ 2019/rust/src/solutions/day02.rs
@@ -22,33 +22,31 @@ impl Solver for Problem {
     fn parse_input<R: io::Read>(&self, r: R) -> Self::Input {
         let r = BufReader::new(r);
         let v : Vec<String> = r.lines().flatten().collect();
-        let mut res = vec![];
-        if let Some(l) = v.iter().next() {
-            res = l.split(",").map(|nb| nb.parse().unwrap()).collect();
+
+        match v.iter().next() {
+            Some(l) => l.split(",").map(|nb| nb.parse().unwrap()).collect(),
+            None => vec![]
         }
-        res
     }
 
     fn solve_first(&self, input: &Self::Input) -> Self::Output1 {
-        part1(input)
+        exec(input, 12, 2)
     }
 
     fn solve_second(&self, input: &Self::Input) -> Self::Output2 {
-        part2(input)
+        find_noun_verb(input, 19690720)
     }
 }
 
-fn part1 (_vec: &Vec<usize>) -> usize {
-    let mut i : usize = 0;
+fn exec (_vec: &Vec<usize>, noun: usize, verb: usize) -> usize {
+    let mut i = 0;
     let mut vec = _vec.clone();
-    vec[1] = 12;
-    vec[2] = 2;
+    vec[1] = noun;
+    vec[2] = verb;
     while i + 4 < vec.len() {
-        let addr1 = vec[i + 1];
-        let addr2 = vec[i + 2];
         let dest = vec[i + 3];
-        let val1 = vec[addr1];
-        let val2 = vec[addr2];
+        let val1 = vec[vec[i + 1]];
+        let val2 = vec[vec[i + 2]];
         match vec[i] {
             1 => vec[dest] = val1 + val2,
             2 => vec[dest] = val1 * val2,
@@ -59,37 +57,22 @@ fn part1 (_vec: &Vec<usize>) -> usize {
     vec[0]
 }
 
-fn part2 (_vec: &Vec<usize>) -> Pair<usize> {
-    let mut noun : usize = 0;
-    let mut verb : usize = 0;
+fn find_noun_verb (vec: &Vec<usize>, goal: usize) -> Pair<usize> {
+    let mut noun = 0;
+    let mut verb = 0;
     let mut stop = false;
-    for n in 0..=99 {
-        for v in 0..=99 {
-            noun = n;
-            verb = v;
-            let mut i : usize = 0;
-            let mut vec = _vec.clone();
-            vec[1] = n;
-            vec[2] = v;
-            while i + 4 < vec.len() {
-                let addr1 = vec[i + 1];
-                let addr2 = vec[i + 2];
-                let dest = vec[i + 3];
-                let val1 = vec[addr1];
-                let val2 = vec[addr2];
-                match vec[i] {
-                    1 => vec[dest] = val1 + val2,
-                    2 => vec[dest] = val1 * val2,
-                    _ => {}
-                }
-                i += 4;
+
+    while noun < 100 && !stop {
+        verb = 0;
+        while verb < 100 && !stop {
+            if exec(vec,noun,verb) == goal {
+                stop = true
             }
-            if vec[0] == 19690720 { stop = true }
-            if stop { break }
+            verb += 1;
         }
-        if stop { break }
+        noun += 1;
     }
-    Pair{ 0:noun, 1:verb }
+    Pair(noun - 1, verb - 1)
 }
 
 // #[cfg(test)]
