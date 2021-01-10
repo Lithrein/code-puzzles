1b44347a03a52d5efcaf56f388e2d554fb8b1fef
Lithrein <lithrein.site@gmail.com>
2021-01-10 17:59:33 +0100

    day03: add part2 (rust)

--- 2019/rust/src/solutions/day03.rs
+++ 2019/rust/src/solutions/day03.rs
@@ -2,7 +2,6 @@ use crate::solver::Solver;
 use std::{
     cmp::Ordering,
     collections::HashSet,
-    fmt,
     io::{self, BufRead, BufReader},
 };
 
@@ -65,37 +64,27 @@ impl<T> Ord for Pair<T> where T: Ord {
 }
 
 impl Solver for Problem {
-    type Input = Pair<Vec<(char,isize)>>;
-    type Output1 = isize;
-    type Output2 = isize;
+    type Input = (Pair<Vec<Pair<isize>>>, Vec<Pair<isize>>);
+    type Output1 = usize;
+    type Output2 = usize;
 
     fn parse_input<R: io::Read>(&self, r: R) -> Self::Input {
         let r = BufReader::new(r);
-        let mut v = r.lines().flatten();
+        let mut v = r.lines().flatten().map(|l|
+                l.split(",")
+                 .map(|item| {
+                     let mut item_iter = item.chars();
+                     let dir = item_iter.next().unwrap();
+                     let val = item_iter.collect::<String>().parse().unwrap();
+                     (dir, val)
+                 }).collect());
 
-        let fst = match v.next() {
-            Some(l) =>
-                l.split(",")
-                 .map(|item| {
-                     let mut item_iter = item.chars();
-                     let dir = item_iter.next().unwrap();
-                     let val = item_iter.collect::<String>().parse().unwrap();
-                     (dir, val)
-                 }).collect(),
-            None => vec![]
-        };
-        let snd = match v.next() {
-            Some(l) =>
-                l.split(",")
-                 .map(|item| {
-                     let mut item_iter = item.chars();
-                     let dir = item_iter.next().unwrap();
-                     let val = item_iter.collect::<String>().parse().unwrap();
-                     (dir, val)
-                 }).collect(),
-            None => vec![]
-        };
-        Pair(fst, snd)
+        let fst = v.next().unwrap_or(vec![]);
+        let snd = v.next().unwrap_or(vec![]);
+        let path1 = path_from_directions(&fst);
+        let path2 = path_from_directions(&snd);
+        let inter = find_intersections(&fst,&snd);
+        (Pair(path1, path2), inter)
     }
 
     fn solve_first(&self, input: &Self::Input) -> Self::Output1 {
@@ -103,34 +92,34 @@ impl Solver for Problem {
     }
 
     fn solve_second(&self, input: &Self::Input) -> Self::Output2 {
-        0
+        part2(&input)
     }
 }
 
-fn path_from_directions(dirs: &Vec<(char,isize)>) -> HashSet<Pair<isize>> {
+fn path_from_directions(dirs: &Vec<(char,isize)>) -> Vec<Pair<isize>> {
     let mut cur_pos = (0,0);
-    let mut path = HashSet::new();
+    let mut path = vec![];
     for dir in dirs {
         match dir {
             ('U', nb) => {
-                for i in 1..=*nb {
+                for _ in 1..=*nb {
                     cur_pos.1 += 1;
-                    path.insert(Pair(cur_pos.0, cur_pos.1));
+                    path.push(Pair(cur_pos.0, cur_pos.1));
                 }},
             ('D', nb) => {
-                for i in 1..=*nb {
+                for _ in 1..=*nb {
                     cur_pos.1 -= 1;
-                    path.insert(Pair(cur_pos.0, cur_pos.1));
+                    path.push(Pair(cur_pos.0, cur_pos.1));
                 }},
             ('L', nb) => {
-                for i in 1..=*nb {
+                for _ in 1..=*nb {
                     cur_pos.0 -= 1;
-                    path.insert(Pair(cur_pos.0, cur_pos.1));
+                    path.push(Pair(cur_pos.0, cur_pos.1));
                 }},
             ('R', nb) => {
-                for i in 1..=*nb {
+                for _ in 1..=*nb {
                     cur_pos.0 += 1;
-                    path.insert(Pair(cur_pos.0, cur_pos.1));
+                    path.push(Pair(cur_pos.0, cur_pos.1));
                 }},
                 _     => {}
         }
@@ -138,16 +127,45 @@ fn path_from_directions(dirs: &Vec<(char,isize)>) -> HashSet<Pair<isize>> {
     path
 }
 
-fn part1(input: &Pair<Vec<(char,isize)>>) -> isize {
-    let path1 = path_from_directions(&input.0);
-    let path2 = path_from_directions(&input.1);
-    let pts:Vec<_> = path1.intersection(&path2).collect();
-    pts.iter().map(|pt| isize::abs(pt.0) + isize::abs(pt.1)).min().unwrap()
+fn find_intersections(dirs1: &Vec<(char,isize)>, dirs2: &Vec<(char,isize)>) -> Vec<Pair<isize>> {
+    let path1:HashSet<_> = path_from_directions(dirs1).into_iter().collect();
+    let path2:HashSet<_> = path_from_directions(dirs2).into_iter().collect();
+    path1.intersection(&path2).copied().collect()
+}
+
+fn manhattan_distance(pt: &Pair<isize>) -> usize {
+    (isize::abs(pt.0) + isize::abs(pt.1)) as usize
+}
+
+fn part1(input: &(Pair<Vec<Pair<isize>>>, Vec<Pair<isize>>)) -> usize {
+    input.1.iter().map(|pt| manhattan_distance(pt)).min().unwrap()
+}
+
+fn path_index<'a,'b>(path: &'a Vec<Pair<isize>>, inters: &'b Vec<Pair<isize>>) -> Vec<(usize, &'a Pair<isize>)> {
+    let mut path_index:Vec<_> = path.iter().enumerate()
+        .filter(|(_,elt)| inters.contains(elt))
+        .collect();
+    path_index.sort_by(|a, b| a.1.cmp(&b.1));
+    path_index
+}
+
+fn part2(input: &(Pair<Vec<Pair<isize>>>, Vec<Pair<isize>>)) -> usize {
+    let path1  = &(input.0).0;
+    let path2  = &(input.0).1;
+    let inters = &input.1;
+
+    let path1_index = path_index(path1, inters);
+    let path2_index = path_index(path2, inters);
+
+    path1_index.iter()
+        .zip(path2_index)
+        .map(|(a,b)| (a.0 + b.0 + 2, a.1))
+        .min_by(|a,b| a.0.cmp(&b.0)).unwrap().0
 }
 
 // #[cfg(test)]
 // mod tests {
-//   use crate::solutions::day02::*;
+//   use crate::solutions::day03::*;
 
 //   #[test]
 //   fn test_part1() {
