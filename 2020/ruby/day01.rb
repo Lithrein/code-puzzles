ba53b3ff4e3353c4a5511f248e65b9725b3f8221
Lithrein <lithrein.site@gmail.com>
2021-02-02 15:41:11 +0100

    day01: remove conflict with day02 (ruby test)

--- 2020/ruby/day01.rb
+++ 2020/ruby/day01.rb
@@ -1,6 +1,6 @@
 #! /usr/bin/env ruby
 
-def part1 lst, goal
+def day01_1 lst, goal
   i, j = 0, lst.length - 1
   cur = lst[i] + lst[j]
   while cur != goal && i < lst.length - 1 && j > 0 do
@@ -14,9 +14,9 @@ def part1 lst, goal
   cur == goal ? [lst[i], lst[j]] : []
 end
 
-def part2 lst, goal
+def day01_2 lst, goal
   lst.length.times do |i|
-    tmp = part1(lst, goal - lst[i])
+    tmp = day01_1(lst, goal - lst[i])
     if !tmp.empty? then
       return [tmp[0], tmp[1], lst[i]]
     end
@@ -28,6 +28,6 @@ if $0 == __FILE__ then
   GOAL = 2020
 
   lst = File.open('../inputs/day01').readlines.map(&:to_i).sort
-  puts part1(lst, GOAL).reduce :*
-  puts part2(lst, GOAL).reduce :*
+  puts day01_1(lst, GOAL).reduce :*
+  puts day01_2(lst, GOAL).reduce :*
 end
--- 2020/ruby/spec/day01_spec.rb
+++ 2020/ruby/spec/day01_spec.rb
@@ -11,33 +11,33 @@ nums_ko = [
   [1760,349,37,270]
 ].map &:sort
 
-RSpec.describe 'part1' do
+RSpec.describe 'day01_1' do
   nums_ok.each do |num|
     it "assert that the list `#{num}` contains 2 numbers which sum is 2020" do
-      res = part1(num,2020).sum
+      res = day01_1(num,2020).sum
       expect(res).to eq(2020)
     end
   end
 
   nums_ko.each do |num|
     it "assert that the list `#{num}` does not contain 2 numbers which sum is 2020" do
-      res = part1(num,2020).sum
+      res = day01_1(num,2020).sum
       expect(res).to eq(0)
     end
   end
 end
 
-RSpec.describe 'part2' do
+RSpec.describe 'day01_2' do
   nums_ok.each do |num|
     it "assert that the list `#{num}` contains 3 numbers which sum is 2020" do
-      res = part2(num,2020).sum
+      res = day01_2(num,2020).sum
       expect(res).to eq(2020)
     end
   end
 
   nums_ko.each do |num|
     it "assert that the list `#{num}` does not contain 3 numbers which sum is 2020" do
-      res = part2(num,2020).sum
+      res = day01_2(num,2020).sum
       expect(res).to eq(0)
     end
   end
