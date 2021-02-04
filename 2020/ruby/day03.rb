34786eb44c0897496f03032042950fda92d63d96
Lithrein <lithrein.site@gmail.com>
2021-02-04 15:39:05 +0100

    day03: main code in separate module

--- 2020/ruby/day03.rb
+++ 2020/ruby/day03.rb
@@ -1,9 +1,28 @@
 #! /usr/bin/env ruby
 
-def toboggan lst, dx, dy = 1
-  lst.each_slice(dy).map.with_index { |l,i| l[0][(dx*i) % l[0].length] }.count ?#
+module Day03
+  extend self
+
+  def toboggan lst, dx, dy = 1
+    lst
+      .each_slice(dy)
+      .map.with_index { |l,i| l[0][(dx*i) % l[0].length] }
+      .count ?#
+  end
+
+  def part1 lst
+    toboggan(lst, 3)
+  end
+
+  def part2 lst
+    [1, 3, 5, 7, [1, 2]]
+      .map { |args| toboggan(lst, *args) }
+      .reduce :*
+  end
 end
 
-lst = File.open('../inputs/day03').readlines.map &:chomp
-puts toboggan(lst,3)
-puts [1, 3, 5, 7, [1, 2]].map { |args| toboggan(lst,*args) }.reduce :*
+if $0 == __FILE__ then
+  lst = File.open('../inputs/day03').readlines.map &:chomp
+  puts "Part 1: #{Day03.part1(lst)}"
+  puts "Part 2: #{Day03.part2(lst)}"
+end
