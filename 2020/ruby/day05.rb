c1a007b0b9def7fb05e426abc2799f2087f3bbb1
Lithrein <lithrein.site@gmail.com>
2021-02-05 18:23:24 +0100

    day05: main code in a separate module

--- 2020/ruby/day05.rb
+++ 2020/ruby/day05.rb
@@ -1,37 +1,46 @@
 #! /usr/bin/env ruby
 
-def seat_id (pass)
-  lower, upper = 0,127
-  (0..6).each do |i|
-    if pass[i] == 'B' then lower = (lower + upper) / 2 + 1 end
-    if pass[i] == 'F' then upper = (lower + upper) / 2 end
-  end
-  row = lower
+module Day05
+  extend self
 
-  lower, upper = 0, 7
-  (7..9).each do |i|
-    if pass[i] == 'R' then lower = (lower + upper) / 2 + 1 end
-    if pass[i] == 'L' then upper = (lower + upper) / 2 end
-  end
-  col = upper
+  def seat_id pass
+    lower, upper = 0,127
+    (0..6).each do |i|
+      if pass[i] == 'B' then lower = (lower + upper) / 2 + 1 end
+      if pass[i] == 'F' then upper = (lower + upper) / 2 end
+    end
+    row = lower
 
-  return row * 8 + col
+    lower, upper = 0, 7
+    (7..9).each do |i|
+      if pass[i] == 'R' then lower = (lower + upper) / 2 + 1 end
+      if pass[i] == 'L' then upper = (lower + upper) / 2 end
+    end
+    col = upper
+
+    return row * 8 + col
+  end
+
+  def find_seat seats
+    last_seat = -1
+    seats.each do |seat|
+      return seat unless seat - last_seat == 1
+      last_seat = seat
+    end
+  end
+
+  def part1 lst
+    lst.map { |id| seat_id(id) }.max
+  end
+
+  def part2 lst
+    find_seat(lst.inject((0..1023).to_a) { |acc, s| acc - [seat_id(s)] })
+  end
 end
 
-def find_seat (seats)
-  last_seat = -1
-  seats.each do |seat|
-    return seat unless seat - last_seat == 1
-    last_seat = seat
-  end
+if $0 == __FILE__ then
+  lst = File.open('../inputs/day05').readlines
+  puts "Part 1: #{Day05.part1 lst}"
+  puts "Part 2: #{Day05.part2 lst}"
 end
 
-part1 = 0
-part2 = (0..1023).entries
-File.open('../inputs/day05').read().each_line do |pass|
-  part1 = [part1, seat_id(pass)].max
-  part2 = part2 - [seat_id(pass)]
-end
-
-puts part1
-puts find_seat(part2)
