f5276a80441ac8e9a7227fb429e9ceefcf4c6e6b
Lithrein <lithrein.site@gmail.com>
2021-02-06 13:30:29 +0100

    day06: main code in a separate module

--- 2020/ruby/day06.rb
+++ 2020/ruby/day06.rb
@@ -1,5 +1,29 @@
 #! /usr/bin/env ruby
 
-input = File.open('../inputs/day06').read().split("\n\n")
-puts input.map(&->x{x.gsub(/\n/,'')}).map(&->x{x.split('')}).map(&:uniq).map(&:length).sum()
-puts input.map(&->x{x.split("\n").map(&->x{x.split('')})}).map(&->x{x.reduce(:&)}).map(&:length).sum()
+module Day06
+  extend self
+
+  def part1 input
+    input
+      .map { |x|
+        x.gsub(/\n/, "")
+         .split("")
+         .uniq }
+      .inject(0) { |sum, v| sum + v.length }
+  end
+
+  def part2 input
+    input
+      .map { |x|
+        x.split("\n")
+         .map { |x| x.split "" }
+         .reduce :& }
+      .inject(0) { |sum, v| sum + v.length }
+  end
+end
+
+if $0 == __FILE__
+  input = File.open('../inputs/day06').read.split "\n\n"
+  puts "Part 1: #{Day06.part1 input}"
+  puts "Part 2: #{Day06.part2 input}"
+end
