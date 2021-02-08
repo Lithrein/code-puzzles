98f8b91188aea9c3efb1c87276c5b6b0fe46c4d4
Lithrein <lithrein.site@gmail.com>
2021-02-08 18:57:21 +0100

    day10: main code in a separate module

--- 2020/ruby/day10.rb
+++ 2020/ruby/day10.rb
@@ -1,23 +1,29 @@
 #! /usr/bin/env ruby
 
-def part1 input
-  chain = ([0] + input + [input.max + 3]).each_cons(2).map {|x,y| y - x}
-  chain.count(1) * chain.count(3)
-end
+module Day10
+  extend self
 
-def part2 input
-  arr = Hash.new 0
-  arr[input.length - 1] = 1
-  (0..input.length-2).reverse_each do |idx|
-    (idx..idx+3).each do |i|
-      if i < input.length then
-        arr[idx] += arr[i] if input[i] - input[idx] <= 3
+  def part1 input
+    chain = ([0] + input + [input.max + 3]).each_cons(2).map {|x,y| y - x}
+    chain.count(1) * chain.count(3)
+  end
+
+  def part2 input
+    arr = Hash.new 0
+    arr[input.length - 1] = 1
+    (0..input.length-2).reverse_each do |idx|
+      (idx..idx+3).each do |i|
+        if i < input.length then
+          arr[idx] += arr[i] if input[i] - input[idx] <= 3
+        end
       end
     end
+    input.map.with_index {|_,idx| input[idx] <= 3 ? arr[idx] : 0}.sum
   end
-  input.map.with_index {|_,idx| input[idx] <= 3 ? arr[idx] : 0}.sum
 end
 
-input = File.open('../inputs/day10').read.lines.map(&:to_i).sort
-p part1(input)
-p part2(input)
+if $0 == __FILE__ then
+  input = File.open('../inputs/day10').read.lines.map(&:to_i).sort
+  puts "Part 1: #{Day10.part1(input)}"
+  puts "Part 2: #{Day10.part2(input)}"
+end
