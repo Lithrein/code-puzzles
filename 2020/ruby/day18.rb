df7646ed9b657432e39dde80d71a9a0d9d66e438
Lithrein <lithrein.site@gmail.com>
2021-02-12 22:56:05 +0100

    day18: main code in a separate module

--- 2020/ruby/day18.rb
+++ 2020/ruby/day18.rb
@@ -1,86 +1,92 @@
 #! /usr/bin/env ruby
 
-def eval_rpn expr
-  nb_nb_in_stack = 0
-  stack = []
-  expr.each do |el|
-    if el == '+' || el == '*' then
-      stack = [el] + stack
-    elsif el.is_a? Integer
-      stack = [el] + stack
-    else
-      stack = eval_rpn(el) + stack
-    end
-    while stack.length > 2 do
-      left = stack.shift
-      right = stack.shift
-      op = stack.shift
-      if left.is_a?(Integer) && right.is_a?(Integer) && (op == '+' || op == '*') then
-        stack = [eval("#{left} #{op} #{right}")] + stack
+module Day18
+  extend self
+
+  def eval_rpn expr
+    nb_nb_in_stack = 0
+    stack = []
+    expr.each do |el|
+      if el == '+' || el == '*' then
+        stack = [el] + stack
+      elsif el.is_a? Integer
+        stack = [el] + stack
       else
-        stack = [left,right,op] + stack
-        break
+        stack = eval_rpn(el) + stack
+      end
+      while stack.length > 2 do
+        left = stack.shift
+        right = stack.shift
+        op = stack.shift
+        if left.is_a?(Integer) && right.is_a?(Integer) && (op == '+' || op == '*') then
+          stack = [eval("#{left} #{op} #{right}")] + stack
+        else
+          stack = [left,right,op] + stack
+          break
+        end
       end
     end
+    stack
   end
-  stack
-end
 
-def postfix_to_prefix_without_precedence lst
-  ops, nums = [], []
+  def postfix_to_prefix_without_precedence lst
+    ops, nums = [], []
 
-  lst.each do |el|
-    if el == '+' || el == '*' then
-      ops += [el]
-    elsif el.is_a? Integer then
-      nums += [el]
-    else # el is a list
-      nums += [postfix_to_prefix_without_precedence(el)]
+    lst.each do |el|
+      if el == '+' || el == '*' then
+        ops += [el]
+      elsif el.is_a? Integer then
+        nums += [el]
+      else # el is a list
+        nums += [postfix_to_prefix_without_precedence(el)]
+      end
     end
+
+    ops.reverse + nums
   end
 
-  ops.reverse + nums
-end
+  def postfix_to_prefix_with_precedence lst
+    ops, nums = [], []
 
-def postfix_to_prefix_with_precedence lst
-  ops, nums = [], []
-
-  lst.each do |el|
-    if el == '+' then
-      left = nums.pop
-      nums += [el, left]
-    elsif el == '*' then
-      ops += [el]
-    elsif el.is_a? Integer then
-      nums += [el]
-    else # el is a list
-      nums += [postfix_to_prefix_with_precedence(el)]
+    lst.each do |el|
+      if el == '+' then
+        left = nums.pop
+        nums += [el, left]
+      elsif el == '*' then
+        ops += [el]
+      elsif el.is_a? Integer then
+        nums += [el]
+      else # el is a list
+        nums += [postfix_to_prefix_with_precedence(el)]
+      end
     end
+
+    ops + nums
   end
 
-  ops + nums
+  def rpn_input input
+    input.map { |str| eval(('('+str+')').gsub('+', "'+'").gsub('*',"'*'").tr('() ', '[],')) }
+  end
+
+  def part1 input
+    rpn_input(input)
+      .map { |lst| postfix_to_prefix_without_precedence lst }
+      .map { |expr| eval_rpn expr }
+      .flatten
+      .sum
+  end
+
+  def part2 input
+    rpn_input(input)
+      .map { |lst| postfix_to_prefix_with_precedence lst }
+      .map { |expr| eval_rpn expr }
+      .flatten
+      .sum
+  end
 end
 
-def rpn_input input
-  input.map { |str| eval(('('+str+')').gsub('+', "'+'").gsub('*',"'*'").tr('() ', '[],')) }
+if $0 == __FILE__ then
+  input = File.open('../inputs/day18').readlines.map(&:chomp)
+  puts "Part 1: #{Day18.part1 input}"
+  puts "Part 2: #{Day18.part2 input}"
 end
-
-def part1 input
-  rpn_input(input)
-    .map { |lst| postfix_to_prefix_without_precedence lst }
-    .map { |expr| eval_rpn expr }
-    .flatten
-    .sum
-end
-
-def part2 input
-  rpn_input(input)
-    .map { |lst| postfix_to_prefix_with_precedence lst }
-    .map { |expr| eval_rpn expr }
-    .flatten
-    .sum
-end
-
-input = File.open('../inputs/day18').readlines.map(&:chomp)
-p part1 input
-p part2 input
