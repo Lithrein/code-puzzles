9841a8cbbc3fbda6449c7fd5b1fbf0db2d5c73ed
Lithrein <lithrein.site@gmail.com>
2021-02-15 15:46:01 +0100

    day21: main code in a separate module

--- 2020/ruby/day21.rb
+++ 2020/ruby/day21.rb
@@ -1,60 +1,59 @@
 #! /usr/bin/env ruby
 
-def find_allergenes input
-  hash = Hash.new []
-  final_assoc = Hash.new []
+module Day21
+  extend self
 
-  input.each do |as,bs|
-    as.each do |a|
-      if hash[a] == [] then
-        hash[a] = bs
-      else
-        hash[a] &= bs
+  def find_allergenes input
+    hash = Hash.new []
+    final_assoc = Hash.new []
+
+    input.each do |as,bs|
+      as.each do |a|
+        if hash[a] == [] then
+          hash[a] = bs
+        else
+          hash[a] &= bs
+        end
       end
     end
-  end
 
-  tmp = hash.select { |k,vs| vs.length == 1 }
-  while tmp != {} do
-    tmp.each do |k, vs|
-       final_assoc[k] = vs[0]
-       hash.keys.each do |k|
-         hash[k] -= [vs[0]]
-       end
-    end
     tmp = hash.select { |k,vs| vs.length == 1 }
+    while tmp != {} do
+      tmp.each do |k, vs|
+        final_assoc[k] = vs[0]
+        hash.keys.each do |k|
+          hash[k] -= [vs[0]]
+        end
+      end
+      tmp = hash.select { |k,vs| vs.length == 1 }
+    end
+    final_assoc
   end
-  final_assoc
+
+  def part1 input, allergenes
+    ingredients = []
+    input.each do |_,bs|
+      ingredients += bs
+    end
+    allergenes.values.each do |v|
+      ingredients -= [v]
+    end
+    ingredients.length
+  end
+
+  def part2 allergenes
+    allergenes.keys.sort.map { |k| allergenes[k] }.join ","
+  end
 end
 
-def part1 input, allergenes
-  ingredients = []
-  input.each do |_,bs|
-    ingredients += bs
-  end
-  allergenes.values.each do |v|
-    ingredients -= [v]
-  end
-  ingredients.length
+if $0 == __FILE__ then
+  input = File.open('../inputs/day21').readlines
+    .map(&:chomp)
+    .map(&->x{x.tr("(),","")})
+    .map(&->x{x.split(" contains ")})
+    .map(&->x{[x[1].split(" "),x[0].split(" ")]})
+
+  allergenes = Day21.find_allergenes(input)
+  puts "Part 1: #{Day21.part1(input, allergenes)}"
+  puts "Part 2: #{Day21.part2(allergenes)}"
 end
-
-def part2 allergenes
-  allergenes.keys.sort.map { |k| allergenes[k] }.join ","
-end
-
-# input = <<-DATA
-# mxmxvkd kfcds sqjhc nhms (contains dairy, fish)
-# trh fvjkl sbzzf mxmxvkd (contains dairy)
-# sqjhc fvjkl (contains soy)
-# sqjhc mxmxvkd sbzzf (contains fish)
-# DATA
-#   .split("\n")
-input = File.open('../inputs/day21').readlines
-  .map(&:chomp)
-  .map(&->x{x.tr("(),","")})
-  .map(&->x{x.split(" contains ")})
-  .map(&->(x,y){[y.split(" "),x.split(" ")]})
-
-allergenes = find_allergenes(input)
-puts part1(input, allergenes)
-puts part2(allergenes)
